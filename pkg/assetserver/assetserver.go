10cf28f38a19f1e9ce9c92c46c71ada9d0189d23
thomasplowman <tomplowy@gmail.com>
2020-12-31 15:41:19 +0000

    Adding postgres driver

--- pkg/assetserver/assetserver.go
+++ pkg/assetserver/assetserver.go
@@ -1,7 +1,17 @@
 package assetserver
 
+import (
+  "gorm.io/gorm"
+  "gorm.io/driver/postgres"
+)
+
 type Asset struct {
+  gorm.Model
   Path string
   ContentType string
   Data string //TODO: convert this to a byte[] dept on GORM conventions/rules
+}
+
+type AssetServer struct {
+  
 }
